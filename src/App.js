4a606d72b7d19b08036aca3e56e350bbe60173dd
Kedrick06 <bfloblk@gmail.com>
2020-09-04 15:44:43 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="hero-image">
+        <h2>BLUE NOTE 80TH ANNIVERSARY BROADCAST</h2><h3>New York</h3>
+        <h4>A full day of radio hosted by some of the label's most notable artists and enthusiasts.</h4>
+      </div>
+      <div className="mixtapes-section">
+        <h2>INFINITE MIXTAPES</h2>
+        <h3>Music-only themed radio streams, made from NOTS resident and guest shows</h3>
+        <div className="mixtape-child">
+          <div className="memorylane">MEMORY LANE</div>
+          <div className="poolside">POOLSIDE</div>
+          <div className="expansions"></div>
+          <div className="hiphop"></div>
+          <div className="slowfocus"></div>
+          <div className="thefloor"></div>
+        </div>
+        
+
+      </div>
     </div>
   );
 }
